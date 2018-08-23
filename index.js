9e005382beb75a7063b972781e983b1d4bd0f700
nancychalen <chalen_na201@outlook.com>
2018-08-23 10:40:24 -0500

    static files

--- index.js
+++ index.js
@@ -5,21 +5,62 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-var exphbs = require('express-handlebars');
-var app = express();
 
-app.engine('handlebars',exphbs({defaultLayout:'main'}));
+var exphbs = require('express-handlebars');
+
+var app=express();
+
+app.engine('handlebars',
+exphbs({defaultLayout: 'main'}));
 
 app.set('view engine', 'handlebars');
 
-app.get('/',function (req,res){
-  res.render('home');
-});
+app.get('/', (req, res) => res.render('home'));
+
+var fortune = require('./lib/fortune');
+//usa modulos de la libreria fortune.js
+  app.get('/about',function(req,res){
+    res.render('about', { fortune: fortune.getFortune() });
+  })
+
+//archivos estaticos
+app.use(express.static(path.join(__dirname,'/public')));
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
