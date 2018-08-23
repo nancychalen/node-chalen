57d8d80b3877958d690604b486d93712e195868d
nancychalen <chalen_na201@outlook.com>
2018-08-23 11:37:55 -0500

    node

--- index.js
+++ index.js
@@ -17,6 +17,27 @@ app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => res.render('home'));
 
+//ruta login route
+app.get('/login', (req, res) => res.render('login',{csrf: 'abc'}));
+
+
+//body parcer
+var bodyParser =require('body-parser');
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.post('/process',function(req,res){
+  console.log('formulario:'+req.query.form);
+  console.log('nombre'+req.body.name);
+  console.log('nombre'+req.body.psw);
+});
+
+
+
+
+
+
+
+
 var fortune = require('./lib/fortune');
 //usa modulos de la libreria fortune.js
   app.get('/about',function(req,res){
@@ -29,6 +50,8 @@ app.use(express.static(path.join(__dirname,'/public')));
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 
+
+//ruta para ir a mi formulario 
 
 
 
